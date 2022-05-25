a3077f4c290cc80c7bdace60188f93903ea94487
Christopher Fry <christopherfry@google.com>
2022-05-25 01:01:36 +0000

    feat: ui 6

--- plugins/cad/src/components/RepositoryPage/components/AdvancedRepositoryOptions.tsx
+++ plugins/cad/src/components/RepositoryPage/components/AdvancedRepositoryOptions.tsx
@@ -57,7 +57,7 @@ export const AdvancedRepositoryOptions = () => {
         <DialogTitle>Unregister repository</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Are you sure you want to unregister this repository?3
+            Are you sure you want to unregister this repository?4
           </DialogContentText>
         </DialogContent>
         <DialogActions>
